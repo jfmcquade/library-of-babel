c4d9c167691e2c8e4e8ad65fe7186892c6efacc7
Johnny McQuade <jfmcquade@hotmail.com>
2023-05-21 15:38:47 +0100

    chore: renamed project wittgenstein-gpt->library-of-babel

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/wittgenstein-gpt'),
+      dir: require('path').join(__dirname, './coverage/library-of-babel'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wittgenstein-gpt'`, () => {
+  it(`should have as title 'library-of-babel'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wittgenstein-gpt');
+    expect(app.title).toEqual('library-of-babel');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wittgenstein-gpt app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('library-of-babel app is running!');
   });
 });
